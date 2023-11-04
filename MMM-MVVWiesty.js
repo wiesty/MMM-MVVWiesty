cdb7d1ff97b096188c4b42a657cb3c77e53bd34f
Wiesty <basti@hypermail.de>
2023-11-04 15:08:28 +0100

    added filters

--- MMM-MVVWiesty.js
+++ MMM-MVVWiesty.js
@@ -1,116 +1,124 @@
 Module.register("MMM-MVVWiesty", {
-  defaults: {
-      updateInterval: 300000,
-      maxEntries: 5,
-      stopId: "de:09162:6"
-  },
+    defaults: {
+        updateInterval: 300000,
+        maxEntries: 5,
+        stopId: "de:09162:6",
+        filter: {}
+    },
 
-  start: function () {
-      this.departures = [];
-      this.loadDepartures();
-      this.scheduleUpdate();
-  },
+    start: function () {
+        this.departures = [];
+        this.loadDepartures();
+        this.scheduleUpdate();
+    },
 
-  getStyles: function () {
-      return ["MMM-MVVWiesty.css"];
-  },
+    getStyles: function () {
+        return ["MMM-MVVWiesty.css"];
+    },
 
-  getHeader: function () {
-      return "MVV Abfahrtsmonitor";
-  },
+    getHeader: function () {
+        return "MVV Abfahrtsmonitor";
+    },
 
-  getDom: function () {
-      var wrapper = document.createElement("div");
+    getDom: function () {
+        var wrapper = document.createElement("div");
 
-      if (this.departures.length > 0) {
-          var table = document.createElement("table");
-          table.classList.add("mvv-table");
+        if (this.departures.length > 0) {
+            var table = document.createElement("table");
+            table.classList.add("mvv-table");
 
-          for (var i = 0; i < this.departures.length && i < this.config.maxEntries; i++) {
-              var departure = this.departures[i];
+            for (var i = 0; i < this.departures.length && i < this.config.maxEntries; i++) {
+                var departure = this.departures[i];
 
-              var row = document.createElement("tr");
+                var row = document.createElement("tr");
 
-              var iconCell = document.createElement("td");
-              iconCell.classList.add("icon-cell");
-              var lineImage = document.createElement("img");
-              lineImage.classList.add("productsvg");
+                var iconCell = document.createElement("td");
+                iconCell.classList.add("icon-cell");
+                var lineImage = document.createElement("img");
+                lineImage.classList.add("productsvg");
 
-              switch (departure.line.name) {
-                  case "Bus":
-                      lineImage.src = this.file("assets/bus.svg");
-                      break;
-                  case "S-Bahn":
-                      lineImage.src = this.file("assets/sbahn.svg");
-                      break;
-                  case "Tram":
-                      lineImage.src = this.file("assets/tram.svg");
-                      break;
-                  case "U-Bahn":
-                      lineImage.src = this.file("assets/ubahn.svg");
-                      break;
-                  case "MVV-Regionalbus":
-                      lineImage.src = this.file("assets/bus.svg");
-                      break;
-                  default:
-                      lineImage.src = this.file("assets/default.svg");
-                      break;
-              }
+                switch (departure.line.name) {
+                    case "Bus": lineImage.src = this.file("assets/bus.svg");
+                        break;
+                    case "S-Bahn": lineImage.src = this.file("assets/sbahn.svg");
+                        break;
+                    case "Tram": lineImage.src = this.file("assets/tram.svg");
+                        break;
+                    case "U-Bahn": lineImage.src = this.file("assets/ubahn.svg");
+                        break;
+                    case "MVV-Regionalbus": lineImage.src = this.file("assets/bus.svg");
+                        break;
+                    default: lineImage.src = this.file("assets/default.svg");
+                        break;
+                }
 
-              iconCell.appendChild(lineImage);
-              row.appendChild(iconCell);
+                iconCell.appendChild(lineImage);
+                row.appendChild(iconCell);
 
-              var lineCell = document.createElement("td");
-              lineCell.innerHTML = departure.line.number;
-              row.appendChild(lineCell);
+                var lineCell = document.createElement("td");
+                lineCell.innerHTML = departure.line.number;
+                row.appendChild(lineCell);
 
-              var directionCell = document.createElement("td");
-              directionCell.innerHTML = departure.line.direction;
-              row.appendChild(directionCell);
+                var directionCell = document.createElement("td");
+                directionCell.innerHTML = departure.line.direction;
+                row.appendChild(directionCell);
 
-              var timeCell = document.createElement("td");
-              timeCell.innerHTML = departure.departureLive;
-              row.appendChild(timeCell);
+                var timeCell = document.createElement("td");
+                timeCell.innerHTML = departure.departureLive;
+                row.appendChild(timeCell);
 
-              table.appendChild(row);
-          }
+                table.appendChild(row);
+            }
 
-          wrapper.appendChild(table);
-      } else {
-          wrapper.innerHTML = "Keine Abfahrten gefunden.";
-      }
+            wrapper.appendChild(table);
+        } else {
+            wrapper.innerHTML = "Keine Abfahrten gefunden.";
+        }
 
-      return wrapper;
-  },
+        return wrapper;
+    },
 
-  loadDepartures: function () {
-      var self = this;
-      var stopId = this.config.stopId.replace(/:/g, "%3A");
-      var url =
-          "https://www.mvv-muenchen.de/?eID=departuresFinder&action=get_departures&stop_id=" +
-          stopId +
-          "&requested_timestamp=" +
-          Math.floor(Date.now() / 1000) +
-          "&lines=";
+    filterDepartures: function (departures) {
+        var self = this;
+        var filterKeys = Object.keys(self.config.filter);
+        if (filterKeys.length === 0 || self.config.filter.hasOwnProperty("all")) {
+            return departures;
+        }
 
-      var xhr = new XMLHttpRequest();
-      xhr.open("GET", url, true);
-      xhr.onreadystatechange = function () {
-          if (xhr.readyState === 4 && xhr.status === 200) {
-              var response = JSON.parse(xhr.responseText);
-              if (response && response.departures && response.departures.length > 0) {
-                  self.departures = response.departures;
-                  self.updateDom();
-              }
-          }
-      };
-      xhr.send();
-  },
+        return departures.filter(function (departure) {
+            var isLineFiltered = self.config.filter.hasOwnProperty(departure.line.number);
+            if (! isLineFiltered) {
+                return false;
+            }
 
-  scheduleUpdate: function () {
-      var self = this;
-      setInterval(function () {
-          self.loadDepartures();
-      }, this.config.updateInterval);
-  }
+            var lineDirectionFilter = self.config.filter[departure.line.number];
+            return lineDirectionFilter === "" || departure.direction === lineDirectionFilter;
+        });
+    },
+
+    loadDepartures: function () {
+        var self = this;
+        var stopId = this.config.stopId.replace(/:/g, "%3A");
+        var url = "https://www.mvv-muenchen.de/?eID=departuresFinder&action=get_departures&stop_id=" + stopId + "&requested_timestamp=" + Math.floor(Date.now() / 1000) + "&lines=";
+
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", url, true);
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                var response = JSON.parse(xhr.responseText);
+                if (response && response.departures && response.departures.length > 0) {
+                    self.departures = self.filterDepartures(response.departures);
+                    self.updateDom();
+                }
+            }
+        };
+        xhr.send();
+    },
+
+    scheduleUpdate: function () {
+        var self = this;
+        setInterval(function () {
+            self.loadDepartures();
+        }, this.config.updateInterval);
+    }
 });
--- README.md
+++ README.md
@@ -29,6 +29,50 @@ MagicMirror² Module to display public transport from Munich  in Germany.
         updateInterval: 300000, // Aktualisierungsintervall in Millisekunden (hier: 5 Minuten)
         maxEntries: 5, // Maximale Anzahl der angezeigten Einträge in der Tabelle
         stopId: "de:09162:6", // Stop-ID im Format de:09162:6
+        filter: {} // Filter Options are "Line Name": "direction", multiple lines possible
     }
 },
 ```
+
+#
+
+### Filter by Line and Direction
+You can filter departures by both line number and direction. To do so, add the line numbers as keys and the desired directions as values in the `filter` object in your module configuration. (Be sure to type the exact name of the line number and for the for the direction. TRAM and BUS only have Numbers. (Tram 16 would be "16", and BUS 123 would be "123")
+
+Example:
+
+
+    filter: {
+      "S8": "Herrsching",
+      "U2": "Messestadt Ost"
+    } 
+
+With the above configuration, the module will only display departures for the S8 line going towards Herrsching and the U2 line towards Messestadt Ost.
+
+### Filter by Line Only
+
+If you want to display departures for certain lines regardless of their direction, simply provide the line number with an empty string as the value. For example:
+
+javascriptCopy code
+
+`filter: {
+  "16": "",
+  "S8": ""
+}` 
+
+This configuration results in the module showing all departures for the 16 and S8 lines, irrespective of their direction.
+
+### Display All Departures
+
+To show all departures without any filtering, you can either leave the `filter` object empty or include the key `all` with an empty string as its value. Both of these configurations will display every departure:
+
+`filter: {
+}` 
+
+Or:
+
+`filter: {
+  "all": ""
+}` 
+
+This flexible filtering system ensures that you can always access the departures that are most relevant to you without any unnecessary clutter.
