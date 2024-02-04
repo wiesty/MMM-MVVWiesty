facc36173b74053b066f1f79355f17c208dfb522
wiesty <basti@hypermail.de>
2024-02-04 20:44:38 +0100

    added custom header option

--- MMM-MVVWiesty.js
+++ MMM-MVVWiesty.js
@@ -3,40 +3,40 @@ Module.register("MMM-MVVWiesty", {
         updateInterval: 300000,
         maxEntries: 5,
         stopId: "de:09162:6",
-        filter: {}
+        filter: {},
     },
-
+  
     start: function () {
         this.departures = [];
         this.loadDepartures();
         this.scheduleUpdate();
     },
-
+  
     getStyles: function () {
         return ["MMM-MVVWiesty.css"];
     },
-
+  
     getHeader: function () {
-        return "MVV Abfahrtsmonitor";
+        return this.config.header && (this.config.header !== "") ? this.config.header : "MVV Abfahrtsmonitor";
     },
-
+  
     getDom: function () {
         var wrapper = document.createElement("div");
-
+  
         if (this.departures.length > 0) {
             var table = document.createElement("table");
             table.classList.add("mvv-table");
-
+  
             for (var i = 0; i < this.departures.length && i < this.config.maxEntries; i++) {
                 var departure = this.departures[i];
-
+  
                 var row = document.createElement("tr");
-
+  
                 var iconCell = document.createElement("td");
                 iconCell.classList.add("icon-cell");
                 var lineImage = document.createElement("img");
                 lineImage.classList.add("productsvg");
-
+  
                 switch (departure.line.name) {
                     case "Bus": lineImage.src = this.file("assets/bus.svg");
                         break;
@@ -51,56 +51,56 @@ Module.register("MMM-MVVWiesty", {
                     default: lineImage.src = this.file("assets/default.svg");
                         break;
                 }
-
+  
                 iconCell.appendChild(lineImage);
                 row.appendChild(iconCell);
-
+  
                 var lineCell = document.createElement("td");
                 lineCell.innerHTML = departure.line.number;
                 row.appendChild(lineCell);
-
+  
                 var directionCell = document.createElement("td");
                 directionCell.innerHTML = departure.line.direction;
                 row.appendChild(directionCell);
-
+  
                 var timeCell = document.createElement("td");
                 timeCell.innerHTML = departure.departureLive;
                 row.appendChild(timeCell);
-
+  
                 table.appendChild(row);
             }
-
+  
             wrapper.appendChild(table);
         } else {
             wrapper.innerHTML = "Keine Abfahrten gefunden.";
         }
-
+  
         return wrapper;
     },
-
+  
     filterDepartures: function (departures) {
         var self = this;
         var filterKeys = Object.keys(self.config.filter);
         if (filterKeys.length === 0 || self.config.filter.hasOwnProperty("all")) {
             return departures;
         }
-
+  
         return departures.filter(function (departure) {
             var isLineFiltered = self.config.filter.hasOwnProperty(departure.line.number);
             if (! isLineFiltered) {
                 return false;
             }
-
+  
             var lineDirectionFilter = self.config.filter[departure.line.number];
             return lineDirectionFilter === "" || departure.direction === lineDirectionFilter;
         });
     },
-
+  
     loadDepartures: function () {
         var self = this;
         var stopId = this.config.stopId.replace(/:/g, "%3A");
         var url = "https://www.mvv-muenchen.de/?eID=departuresFinder&action=get_departures&stop_id=" + stopId + "&requested_timestamp=" + Math.floor(Date.now() / 1000) + "&lines=";
-
+  
         var xhr = new XMLHttpRequest();
         xhr.open("GET", url, true);
         xhr.onreadystatechange = function () {
@@ -114,11 +114,12 @@ Module.register("MMM-MVVWiesty", {
         };
         xhr.send();
     },
-
+  
     scheduleUpdate: function () {
         var self = this;
         setInterval(function () {
             self.loadDepartures();
         }, this.config.updateInterval);
     }
-});
+  });
+  
